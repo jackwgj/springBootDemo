24ad246714b7f921726e22849a7fb28a5d6184a6
王高节 <jack_wgj@163.com>
2017-06-22 02:23:40 +0800

    springBootDemo

---
+++ src/main/java/hello/SampleController.java
@@ -0,0 +1,26 @@
+package hello;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/hello/{myName}")
+    String index(@PathVariable String myName) {
+        return "Hello "+myName+"!!!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
